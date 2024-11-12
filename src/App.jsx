fba24d6dfc2c28502119ca2e4a56e086443e35df
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-11-12 21:30:17 +0600

    destructring props , props validate and added Device component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import PropTypes from 'prop-types';
 
 function App() {
 
@@ -6,12 +7,20 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <Persone></Persone>
-      <Persone></Persone>
-      <Persone></Persone>
-      <Student></Student>
+      <Device name="laptop" price="55k"></Device>
+      <Device name="phone" price="30k"></Device>
+      <Student name="Abid" age="20"></Student>
+      <Student name="Hridoy" age="21"></Student>
+      <Student name="Rifat" age="22"></Student>
+      <Developer></Developer>
     </>
   )
 }
+
+function Device(props) {
+  return <h3>This device : {props.name} Price : {props.price}</h3>
+}
+
 
 function Persone() {
   const age = 35;
@@ -21,13 +30,38 @@ function Persone() {
   )
 }
 
-function Student() {
+function Student(std) {
+  console.log(std)
+  const { name, age } = std;
+
   return (
-    <div className="">
+    <div style={{ border: '2px solid tomato', borderRadius: '20px', margin: '20px', padding: '10px' }}>
       <h4>This is a student</h4>
-      <p>Name:</p>
-      <p>age:</p>
+      <p>Name: {name}</p>
+      <p>age: {age}</p>
+    </div>
+  )
+};
+
+function Developer() {
+  const devStyle = {
+    border: '2px solid AliceBlue',
+    margin: '20px',
+    padding: '15px',
+    borderRadius: '20px'
+  }
+  return (
+    <div style={devStyle}>
+      <h4>Developer</h4>
+      <p>Coding:</p>
     </div>
   )
 }
+
+// validating props
+Device.propTypes = {
+  name: PropTypes.object,
+  price: PropTypes.object
+}
+
 export default App
